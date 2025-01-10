b475cb9139af278e4f2f1728f8dc5252f524f50f
nicatibra <bd7xw445u@code.edu.az>
2025-01-10 17:49:12 +0400

    TestPraktika

--- src/Infrastructure/PrroniaOnion.Infrastructure/obj/Debug/net8.0/PrroniaOnion.Infrastructure.AssemblyInfo.cs
+++ src/Infrastructure/PrroniaOnion.Infrastructure/obj/Debug/net8.0/PrroniaOnion.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrroniaOnion.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6b2a7c49ab938e5687c2c5486d36daf2c7e0634")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrroniaOnion.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrroniaOnion.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
